0458e3c144369eade8f8c663927390c42ce69735
benthekatz <benthekatz@gmail.com>
2018-02-27 13:46:51 -0500

    RESTful Web Service

--- ben-server-master/benserver/src/main/java/hello/GreetingController.java
+++ ben-server-master/benserver/src/main/java/hello/GreetingController.java
@@ -1,17 +1,22 @@
 package hello;
 
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class GreetingController {
 
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
     @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
     }
 
 }
